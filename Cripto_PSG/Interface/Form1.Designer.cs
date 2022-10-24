70e4a1a10fc440a8fcd42f27c24e6a0e98a8c1ff
Barretim <bruno.barreto20013@gmail.com>
2022-10-24 11:41:49 -0400

    Ajustes Layout

--- Cripto_PSG/Interface/Form1.Designer.cs
+++ Cripto_PSG/Interface/Form1.Designer.cs
@@ -45,11 +45,13 @@
             // 
             this.txt_chave.AccessibleDescription = "bla";
             this.txt_chave.AccessibleName = "Digite um número de 1 a 9.";
-            this.txt_chave.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.txt_chave.BackColor = System.Drawing.SystemColors.InfoText;
+            this.txt_chave.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_chave.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(169)))), ((int)(((byte)(153)))));
             this.txt_chave.Location = new System.Drawing.Point(12, 43);
             this.txt_chave.MaxLength = 2;
             this.txt_chave.Name = "txt_chave";
-            this.txt_chave.Size = new System.Drawing.Size(40, 20);
+            this.txt_chave.Size = new System.Drawing.Size(40, 24);
             this.txt_chave.TabIndex = 1;
             this.txt_chave.Tag = "";
             this.txt_chave.TextChanged += new System.EventHandler(this.txt_chave_TextChanged);
@@ -61,7 +63,7 @@
             this.btn_criptar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
             this.btn_criptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_criptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_criptar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(169)))), ((int)(((byte)(153)))));
+            this.btn_criptar.ForeColor = System.Drawing.Color.MistyRose;
             this.btn_criptar.Location = new System.Drawing.Point(12, 272);
             this.btn_criptar.Name = "btn_criptar";
             this.btn_criptar.Size = new System.Drawing.Size(110, 30);
@@ -75,7 +77,7 @@
             // 
             this.lbl_1.AutoSize = true;
             this.lbl_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(168)))), ((int)(((byte)(153)))));
+            this.lbl_1.ForeColor = System.Drawing.Color.MistyRose;
             this.lbl_1.Location = new System.Drawing.Point(9, 9);
             this.lbl_1.Name = "lbl_1";
             this.lbl_1.Size = new System.Drawing.Size(276, 18);
@@ -84,25 +86,30 @@
             // 
             // txt_cripto
             // 
-            this.txt_cripto.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txt_cripto.BackColor = System.Drawing.SystemColors.Desktop;
+            this.txt_cripto.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_cripto.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(169)))), ((int)(((byte)(153)))));
             this.txt_cripto.Location = new System.Drawing.Point(12, 149);
             this.txt_cripto.Multiline = true;
             this.txt_cripto.Name = "txt_cripto";
             this.txt_cripto.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txt_cripto.Size = new System.Drawing.Size(428, 97);
+            this.txt_cripto.Size = new System.Drawing.Size(477, 97);
             this.txt_cripto.TabIndex = 2;
             this.txt_cripto.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.btn_criptar_KeyPress);
             // 
             // txt_decripto
             // 
+            this.txt_decripto.BackColor = System.Drawing.Color.Black;
             this.txt_decripto.CausesValidation = false;
             this.txt_decripto.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.txt_decripto.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_decripto.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(169)))), ((int)(((byte)(153)))));
             this.txt_decripto.Location = new System.Drawing.Point(12, 362);
             this.txt_decripto.Multiline = true;
             this.txt_decripto.Name = "txt_decripto";
             this.txt_decripto.ReadOnly = true;
             this.txt_decripto.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txt_decripto.Size = new System.Drawing.Size(428, 90);
+            this.txt_decripto.Size = new System.Drawing.Size(477, 97);
             this.txt_decripto.TabIndex = 7;
             // 
             // btn_limpar
@@ -111,8 +118,8 @@
             this.btn_limpar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
             this.btn_limpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_limpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_limpar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(169)))), ((int)(((byte)(153)))));
-            this.btn_limpar.Location = new System.Drawing.Point(330, 272);
+            this.btn_limpar.ForeColor = System.Drawing.Color.Silver;
+            this.btn_limpar.Location = new System.Drawing.Point(379, 272);
             this.btn_limpar.Name = "btn_limpar";
             this.btn_limpar.Size = new System.Drawing.Size(110, 30);
             this.btn_limpar.TabIndex = 5;
@@ -126,8 +133,8 @@
             this.btn_sair.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
             this.btn_sair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_sair.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_sair.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(169)))), ((int)(((byte)(153)))));
-            this.btn_sair.Location = new System.Drawing.Point(330, 483);
+            this.btn_sair.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(2)))), ((int)(((byte)(2)))));
+            this.btn_sair.Location = new System.Drawing.Point(379, 487);
             this.btn_sair.Name = "btn_sair";
             this.btn_sair.Size = new System.Drawing.Size(110, 30);
             this.btn_sair.TabIndex = 6;
@@ -141,8 +148,8 @@
             this.btn_decriptar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
             this.btn_decriptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_decriptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_decriptar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(169)))), ((int)(((byte)(153)))));
-            this.btn_decriptar.Location = new System.Drawing.Point(175, 272);
+            this.btn_decriptar.ForeColor = System.Drawing.Color.MistyRose;
+            this.btn_decriptar.Location = new System.Drawing.Point(193, 272);
             this.btn_decriptar.Name = "btn_decriptar";
             this.btn_decriptar.Size = new System.Drawing.Size(110, 30);
             this.btn_decriptar.TabIndex = 8;
@@ -154,7 +161,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(168)))), ((int)(((byte)(153)))));
+            this.label1.ForeColor = System.Drawing.Color.MistyRose;
             this.label1.Location = new System.Drawing.Point(9, 89);
             this.label1.MaximumSize = new System.Drawing.Size(400, 0);
             this.label1.Name = "label1";
@@ -166,7 +173,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(168)))), ((int)(((byte)(153)))));
+            this.label2.ForeColor = System.Drawing.Color.MistyRose;
             this.label2.Location = new System.Drawing.Point(9, 325);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(89, 18);
@@ -178,8 +185,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(46)))), ((int)(((byte)(59)))));
-            this.ClientSize = new System.Drawing.Size(472, 558);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(23)))), ((int)(((byte)(25)))));
+            this.ClientSize = new System.Drawing.Size(516, 558);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_decriptar);
